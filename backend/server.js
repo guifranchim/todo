eb8f87e58325afe9894c8d62cffe6d052b5e14ec
Guilherme Franchin <gui.franchin12@gmail.com>
2025-04-08 23:54:08 -0300

    fisr commit

---
+++ backend/server.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const mysql = require('mysql2');
+const { v4: uuidv4 } = require('uuid');
+const cors = require('cors');
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'rootpassword',
+  database: process.env.DB_NAME || 'tasks_db',
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log(err)
+    console.error('Erro ao conectar ao banco de dados:', err.stack);
+    return;
+  }
+  console.log('Conectado ao banco de dados');
+});
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/tasks', (req, res) => {
+  db.query('SELECT * FROM tasks', (err, results) => {
+    if (err) {
+      console.log(err)
+      return res.status(500).json({ error: 'Erro ao obter as tarefas' });
+    }
+    res.json(results);
+  });
+});
+
+
+app.post('/tasks', (req, res) => {
+  const { description, isDone } = req.body;
+  const id = uuidv4();
+
+  const query = 'INSERT INTO tasks (id, description, isDone) VALUES (?, ?, ?)';
+  db.query(query, [id, description, isDone], (err, result) => {
+    if (err) {
+      console.log(err)
+      return res.status(500).json({ error: 'Erro ao criar a tarefa' });
+    }
+    res.status(201).json({ id, description, isDone });
+  });
+});
+
+
+app.delete('/tasks/:id', (req, res) => {
+  const { id } = req.params;
+  db.query('DELETE FROM tasks WHERE id = ?', [id], (err, result) => {
+    if (err) {
+      console.log(err)
+      return res.status(500).json({ error: 'Erro ao excluir a tarefa' });
+    }
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: 'Tarefa não encontrada' });
+    }
+    res.status(204).send();
+  });
+});
+
+
+app.patch('/tasks/:id', (req, res) => {
+  const { id } = req.params;
+  const { isDone } = req.body;
+
+  db.query('UPDATE tasks SET isDone = ? WHERE id = ?', [isDone, id], (err, result) => {
+    if (err) {
+      console.log(err)
+      return res.status(500).json({ error: 'Erro ao atualizar a tarefa' });
+    }
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: 'Tarefa não encontrada' });
+    }
+    db.query('SELECT * FROM tasks WHERE id = ?', [id], (err, rows) => {
+      if (err) {
+        console.log(err)
+        return res.status(500).json({ error: 'Erro ao recuperar a tarefa atualizada' });
+      }
+      res.json(rows[0]);
+    });
+  });
+});
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
---
+++ src/main.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable react-refresh/only-export-components */
+import React, { lazy, Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import "./global.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const LandingPage = lazy(() => import("./pages/LandingPage"));
+const ToDo = lazy(() => import("./pages/ToDo"));
+const NotFound = lazy(() => import("./components/NotFound"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Suspense fallback={<div>Loading...</div>}>
+        <LandingPage />
+      </Suspense>
+    ),
+    errorElement: (
+      <Suspense fallback={<div>Loading...</div>}>
+        <NotFound />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/to-do",
+    element: (
+      <Suspense fallback={<div>Loading...</div>}>
+        <ToDo />
+      </Suspense>
+    ),
+    errorElement: (
+      <Suspense fallback={<div>Loading...</div>}>
+        <NotFound />
+      </Suspense>
+    ),
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  // <React.StrictMode>
+    <RouterProvider router={router} />
+  // </React.StrictMode>
+);
