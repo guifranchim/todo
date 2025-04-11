4d07ce1a481c8a99cd33902c0d04dd932c8a98be
Guilherme Franchin <gui.franchin12@gmail.com>
2025-04-10 23:16:46 -0300

    adding dockerfile

--- src/configs/api/index.ts
+++ src/configs/api/index.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const apiRequest = async <T>(config: AxiosRequestConfig): Promise<T> => {
