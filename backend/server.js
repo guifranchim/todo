89ea53b64e94c7de264973e849a2639cd474ae0d
Guilherme Franchin <gui.franchin12@gmail.com>
2025-04-11 01:27:36 -0300

    fix port

--- backend/server.js
+++ backend/server.js
@@ -89,7 +89,7 @@ app.patch('/tasks/:id', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
