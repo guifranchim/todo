2dd36b83b55c49e2f5bf4b6513ed0515c851125c
Ana Julia Santiago <97199596+AnajuSant@users.noreply.github.com>
2025-04-24 21:00:49 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,12 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME || 'tasks_db',
 });
 
+console.log({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+})
 db.connect((err) => {
   if (err) {
     console.log(err)
