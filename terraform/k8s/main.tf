a1331d5536663a52310bc46a2eb1068ae8c9d9bd
Guilherme Franchin <gui.franchin12@gmail.com>
2025-06-16 11:16:48 -0300

    teste

--- terraform/k8s/main.tf
+++ terraform/k8s/main.tf
@@ -71,6 +71,7 @@ resource "google_container_cluster" "primary" {
   location = var.gcp_region
   network  = var.network_name
   remove_default_node_pool = true
+  initial_node_count       = 1
   deletion_protection      = false
 
     node_config {
