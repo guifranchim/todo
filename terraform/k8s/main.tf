903a43f8a8adba748a7ec47506b31e5a69986186
Guilherme Franchin <gui.franchin12@gmail.com>
2025-06-15 21:24:07 -0300

    a

--- terraform/k8s/main.tf
+++ terraform/k8s/main.tf
@@ -73,6 +73,15 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   initial_node_count       = 1
 
+    node_config {
+    machine_type = var.machine_type
+    disk_size_gb = 30
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+
   depends_on = [
     google_storage_bucket.tf_state,
   ]
