a816985fc07cbedbad80452b3b3782ebcdf87bf4
Guilherme Franchin <gui.franchin12@gmail.com>
2025-06-16 23:07:55 -0300

    .

--- terraform-k8s/main.tf
+++ terraform-k8s/main.tf
@@ -25,7 +25,7 @@ resource "google_container_node_pool" "primary_nodes" {
 
   node_config {
     machine_type = var.machine_type
-    disk_size_gb = 30
+    disk_size_gb = 20
     oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 
